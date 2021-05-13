d3210176f04d990f7237bd26365f8758b0d56312
git-leon <xleonhunter@gmail.com>
2021-05-13 09:06:43 -0400

    further simplified

--- utils.js
+++
@@ -1,8 +0,0 @@
-// import { ask } from 'stdio';
-// export async function getUserInput (message) {
-//     let userInput = await ask(message);
-//     return userInput;
-// }
-function createRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
